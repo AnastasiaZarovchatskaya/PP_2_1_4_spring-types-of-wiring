63189c732b09e0de18f3333f39e65f1b4bbc550d
Nastya <nastya_zarovchatskaya@mail.ru>
2024-04-08 20:01:30 +0600

    реализация без @Configuration и @Component

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,11 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    private Egg6 egg6;
     @Autowired
-    public void setEgg6(Egg6 egg6) {
-        this.egg6 = egg6;
-    }
+    private Egg6 egg6;
 
     @Override
     public String toString() {
